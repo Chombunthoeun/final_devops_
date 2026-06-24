b84c963164b697bc7ccbdcd5f5bb6b9a770f25db
Mes Lyhor <lyhormes@gmail.com>
2026-06-24 11:02:34 +0700

    i develop models Teacher.php

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    protected $table = 'teacher';
+    protected $table = 'teachers';
     protected $primaryKey = 'id';
     protected $fillable = ['name', 'address', 'mobile'];
     use HasFactory;
