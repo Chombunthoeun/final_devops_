5e59e28e9b4de302339e11f2e416a2edfd0c3cd7
Mes Lyhor <lyhormes@gmail.com>
2026-06-24 10:51:02 +0700

    i develop backends model course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    protected $table = 'courses';
+    protected $table = 'course';
     protected $primaryKey = 'id';
     protected $fillable = [
                             'name',
