3af1b82c20ec6082f6ab25c97d7752b1f79f7f10
narathtin66-aub <narathtin66@gmail.com>
2026-06-25 10:18:47 +0700

    i wrote edit.blade.php for batches

--- resources/views/batches/edit.blade.php
+++ resources/views/batches/edit.blade.php
@@ -3,7 +3,7 @@
 
 <div class="card">
     <div class="card-header">
-        <h4>Edit Batches</h4>
+        <h4>Edit Batche</h4>
     </div>
     <div class="card-body">
         <form action="{{ url('batches/' . $batch->id) }}"
