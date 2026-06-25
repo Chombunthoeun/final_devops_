7667024dc1e446a176a01636a7759b419bc8e576
narathtin66-aub <narathtin66@gmail.com>
2026-06-25 10:14:27 +0700

    i wrote edit.blade.php for course

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
  
 <div class="card">
-  <div class="card-header">Edit Page</div>
+  <div class="card-header">Edit Pages</div>
   <div class="card-body">
       
       <form action="{{ url('courses/' .$courses->id) }}" method="post">
